f5d45277752ffeda85b1b6feae3baceca1dd9a90
machnmb_ <mannybiz@outlook.com>
2023-10-06 09:22:16 -0500

    adjusted the height of the cta

--- retrogold-ecommerce/pages/index.js
+++ retrogold-ecommerce/pages/index.js
@@ -26,7 +26,7 @@ export default function Home({hero, quote, blog, calltoAction}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
+      <main className="h-fit">
         <>
           {isOpenMenu && <RespMenu/>}
           <Navigation/>
