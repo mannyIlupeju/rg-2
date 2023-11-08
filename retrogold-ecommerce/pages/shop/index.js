a8e9481f5d46b677590c5fdac017ea8a0b0c36f4
machnmb_ <mannybiz@outlook.com>
2023-11-08 10:03:38 -0600

    shop page grid

--- retrogold-ecommerce/pages/shop/index.js
+++ retrogold-ecommerce/pages/shop/index.js
@@ -38,7 +38,7 @@ const Shop = ({ shop }) => {
       {isUserRegistered && <Register />}
 
       <main className="section-background">
-        <div className="container mx-auto px-6 py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
+        <div className="container mx-auto px-6 py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
           {shop.map((shopItems) => {
             const { brandName, _id, slug, mainImage, mainImage2, productName, price, shortDescription } = shopItems;
             return (
